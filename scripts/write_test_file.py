5a0e1d376a9b694abddc66f6e087a573f40e013a
Eduard Lemmer <drdlemmer@gmail.com>
2020-04-13 18:46:20 +0300

    Data value change (#11)

--- scripts/write_test_file.py
+++ scripts/write_test_file.py
@@ -8,8 +8,7 @@ db.file.insert_one(
     {
         'id': 0,
         'name': 'test_file',
-        'data': ("{id: 0,type: 1,name: 'some name',data: [{"
-                 "id: '_0',key: 'version',value: 3,required: true},"
+        'data': ("[{id: '_0',key: 'version',value: 3,required: true},"
                  "{id: '_1',key: 'services',value: [{id: '_1_0',key: 'redis',"
                  "value: [{id: '_1_0_0',key: 'build',value: '.'},{id: '_1_0_1',"
                  "key: 'port',value: 8080}]},{id: '_1_1',key: 'mongo',"
