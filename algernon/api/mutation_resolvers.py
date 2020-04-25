2b39f94c013bf71141bd7be4cc877f2fc8ed3fdc
Eduard Lemmer <drdlemmer@gmail.com>
2020-04-25 15:41:28 +0300

    Add updateFile mutation (#20)
    * Add updateFile mutation and it's input
    
    * Add updateFile resolver

--- algernon/api/mutation_resolvers.py
+++ algernon/api/mutation_resolvers.py
@@ -62,3 +62,57 @@ async def resolve_mutation_delete_file(parent, args, ctx, system):
     db.file.delete_one({'id': args['id']})
 
     return document
+
+
+@Resolver('Mutation.updateFile')
+async def resolve_mutation_update_file(parent, args, ctx, system):
+    """
+    Resolve mutation updateFile in GraphqQL API.
+
+    Args:
+        parent: initial field in 'execute' method
+        args: argument for query
+        ctx: context field
+        system: information related to the execution
+
+    Raises:
+        Exception: If 'id' of document not found
+        Exception: If new data not input
+
+    Returns:
+        Updated document
+    """
+    db = ctx['req'].app['db']
+
+    args_input = args['input']
+
+    doc_base = {
+        'name': None,
+        'configType': None,
+        'data': None,
+    }
+
+    doc_base['name'] = args_input.get('name')
+    doc_base['configType'] = args_input.get('configType')
+    doc_base['data'] = args_input.get('data')
+
+    if all(file_value is None for file_value in doc_base.values()):
+        raise Exception(
+            "You should provide at least one value: 'name'" +
+            "'type' or 'data'",
+        )
+
+    doc_base = {
+        key: input_value for key, input_value in doc_base.items()
+        if input_value is not None
+    }
+
+    if await db.file.find_one({'id': args_input['id']}) is None:
+        raise Exception('File does not exist.')
+
+    await db.file.update_one(
+        {'id': args_input['id']},
+        {'$set': doc_base},
+    )
+
+    return await db.file.find_one({'id': args_input['id']})
