eba47f361fdc32953c0f0e7d51f3d7c84aafeab2
Eduard Lemmer <drdlemmer@gmail.com>
2020-04-25 13:04:03 +0300

    Add deleteFile mutation (#19)

--- algernon/api/mutation_resolvers.py
+++ algernon/api/mutation_resolvers.py
@@ -33,3 +33,32 @@ async def resolve_mutation_create_file(parent, args, ctx, system):
     )
 
     return await db['file'].find_one({'_id': ObjectId(document.inserted_id)})
+
+
+@Resolver('Mutation.deleteFile')
+async def resolve_mutation_delete_file(parent, args, ctx, system):
+    """
+    Resolve mutation deleteFile in GraphqQL API.
+
+    Args:
+        parent: initial field in 'execute' method
+        args: argument for query
+        ctx: context field
+        system: information related to the execution
+
+    Raises:
+        Exception: If 'id' of document not found
+
+    Returns:
+        Removed document
+    """
+    db = ctx['req'].app['db']
+
+    document = await db.file.find_one({'id': args['id']})
+
+    if document is None:
+        raise Exception('File does not exist.')
+
+    db.file.delete_one({'id': args['id']})
+
+    return document
